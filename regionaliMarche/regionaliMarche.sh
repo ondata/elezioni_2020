31749f411ce578ea53ebf7e52a9cf501c13fcb94
Andrea Borruso <aborruso@gmail.com>
2020-10-11 11:08:56 +0200

    estratti dati per tuttii comuni e fatto il merge

--- regionaliMarche/regionaliMarche.sh
+++ regionaliMarche/regionaliMarche.sh
@@ -9,30 +9,59 @@ mkdir -p "$folder"/rawdata
 mkdir -p "$folder"/processing
 mkdir -p "$folder"/resources
 
+# scarica dati di base
+
+mlr --c2n cut -f value "$folder"/resources/listaComuni.csv >"$folder"/processing/tmpLista
+
+download="no"
+
+if [[ "$download" == "yes" ]]; then
+  while IFS=$'\t' read -r id; do
+    curl -kL "https://dati.elezioni.marche.it/static_json/liste_0_$id.json" | jq . >"$folder"/rawdata/liste_0_"$id".json
+  done <"$folder"/processing/tmpLista
+fi
+
 ### liste ###
 
-curl "https://dati.elezioni.marche.it/static_json/liste_0_1.json" | jq . >"$folder"/rawdata/liste_0_1.json
+for i in {1..228}; do
 
-# anagrCandidati
+  # anagrCandidati
 
-jq <"$folder"/rawdata/liste_0_1.json '.anagrCandidati' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then label listaDelete,candidatoDelete,item,value then reshape -s item,value then cut -x -r -f "Delete" then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-anagrCandidati.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.anagrCandidati' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then label listaDelete,candidatoDelete,item,value then reshape -s item,value then cut -x -r -f "Delete" then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-anagrCandidati.csv
 
-# totSezioni
+  # totSezioni
 
-jq <"$folder"/rawdata/liste_0_1.json '.totSezioni' | mlr --j2c unsparsify then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-totSezioni.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.totSezioni' | mlr --j2c unsparsify then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-totSezioni.csv
 
-# votiCandidati
+  # votiCandidati
 
-jq <"$folder"/rawdata/liste_0_1.json '.votiCandidati[]' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then cut -x -f item,value then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-votiCandidati.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.votiCandidati[]' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then cut -x -f item,value then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_"$i"-votiCandidati.csv
 
-jq <"$folder"/rawdata/liste_0_1.json '.votiCandidati[].arrVotiCandidato' | mlr --j2c unsparsify then cat -n then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_2,value then rename item_1,id then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-votiCandidati-arrVotiCandidato.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.votiCandidati[].arrVotiCandidato' | mlr --j2c unsparsify then cat -n then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_2,value then rename item_1,id then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-votiCandidati-arrVotiCandidato.csv
 
-# votiListe
+  # votiListe
 
-jq <"$folder"/rawdata/liste_0_1.json '.votiListe' | mlr --j2c unsparsify then cut -x -r -f ":" then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-votiListe.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.votiListe' | mlr --j2c unsparsify then cut -x -r -f ":" then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-votiListe.csv
 
-jq <"$folder"/rawdata/liste_0_1.json '.votiListe.arrVotiListe' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_2,value then rename item_1,id then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-votiListe-arrVotiListe.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.votiListe.arrVotiListe' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then reshape -s item_2,value then rename item_1,id then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-votiListe-arrVotiListe.csv
 
-# anagrListe
+  # anagrListe
 
-jq <"$folder"/rawdata/liste_0_1.json '.anagrListe' | mlr --j2c unsparsify then put -S '$idComune="1"' >"$folder"/rawdata/liste_0_1-anagrListe.csv
+  jq <"$folder"/rawdata/liste_0_"$i".json '.anagrListe' | mlr --j2c unsparsify then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/liste_0_"$i"-anagrListe.csv
+
+done
+
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-anagrCandidati.csv >"$folder"/processing/anagrCandidati.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-totSezioni.csv >"$folder"/processing/totSezioni.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-votiCandidati.csv >"$folder"/processing/votiCandidati.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-votiCandidati-arrVotiCandidato.csv >"$folder"/processing/votiCandidati-arrVotiCandidato.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-votiListe.csv >"$folder"/processing/votiListe.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-votiListe-arrVotiListe.csv >"$folder"/processing/votiListe-arrVotiListe.csv
+
+mlr --csv cat "$folder"/rawdata/liste_0_*-anagrListe.csv >"$folder"/processing/anagrListe.csv
