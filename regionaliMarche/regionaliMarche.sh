5ab8c3a12bd3538e3850c914d3256586e7c65f9e
Andrea Borruso <aborruso@gmail.com>
2020-10-11 18:28:05 +0200

    close #3

--- regionaliMarche/regionaliMarche.sh
+++ regionaliMarche/regionaliMarche.sh
@@ -51,7 +51,6 @@ for i in {1..228}; do
 
 done
 
-
 mlr --csv cat "$folder"/rawdata/liste_0_*-anagrCandidati.csv >"$folder"/processing/anagrCandidati.csv
 
 mlr --csv cat "$folder"/rawdata/liste_0_*-totSezioni.csv >"$folder"/processing/totSezioni.csv
@@ -65,3 +64,17 @@ mlr --csv cat "$folder"/rawdata/liste_0_*-votiListe.csv >"$folder"/processing/vo
 mlr --csv cat "$folder"/rawdata/liste_0_*-votiListe-arrVotiListe.csv >"$folder"/processing/votiListe-arrVotiListe.csv
 
 mlr --csv cat "$folder"/rawdata/liste_0_*-anagrListe.csv >"$folder"/processing/anagrListe.csv
+
+# vedi issue #3
+
+for i in {1..228}; do
+  curl -kL "https://dati.elezioni.marche.it/static_json/raggrup_0_$i.json" | jq . >"$folder"/rawdata/raggrup_0_"$i".json
+done
+
+jq <"$folder"/rawdata/raggrup_0_100.json '.anagrRaggrup' | mlr --j2c unsparsify then cut -x -r -f ":" then cat -n >"$folder"/processing/anagrRaggrup.csv
+
+for i in {1..228}; do
+  jq <"$folder"/rawdata/raggrup_0_"$i".json '.votiRaggrup.arrVotiRaggrup' | mlr --j2c unsparsify then reshape -r '[:]' -o item,value then filter -S '$value=~".+"' then nest --explode --values --across-fields --nested-fs ":" -f item then rename item_1,id then reshape -s item_2,value then put -S '$idComune="'"$i"'"' >"$folder"/rawdata/raggrup_0_"$i"-votiRaggrup-arrVotiRaggrup.csv
+done
+
+mlr --csv cat "$folder"/rawdata/raggrup_0_*-votiRaggrup-arrVotiRaggrup.csv >"$folder"/processing/votiRaggrup-arrVotiRaggrup.csv
